00123d1fabe6fe3b31bb43195606c7d30fdfae0a
SamB440 <sam@islandearth.net>
2021-06-27 21:41:59 +0100

    Fix delay in AnvilGUI showing, disallow inventory or movement actions

--- src/main/java/net/islandearth/anvillogin/listeners/PlayerListener.java
+++ src/main/java/net/islandearth/anvillogin/listeners/PlayerListener.java
@@ -11,7 +11,9 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -33,65 +35,78 @@ public class PlayerListener implements Listener {
                 return;
             }
 
-            Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                if (plugin.getConfig().getBoolean("fastlogin")) {
-                    if (Bukkit.getPluginManager().getPlugin("FastLogin") != null) {
-                        FastLoginBukkit fastLogin = (FastLoginBukkit) Bukkit.getPluginManager().getPlugin("FastLogin");
-                        if (fastLogin != null) {
-                            PremiumStatus premiumStatus = fastLogin.getStatus(myPlayer.getUniqueId());
-                            if (premiumStatus == PremiumStatus.PREMIUM) {
-                                if (plugin.debug()) {
-                                    plugin.getLogger().info("Skipping player " + myPlayer.getName() + " because they are premium.");
-                                }
-                                return;
+            if (plugin.getConfig().getBoolean("fastlogin")) {
+                if (Bukkit.getPluginManager().getPlugin("FastLogin") != null) {
+                    FastLoginBukkit fastLogin = (FastLoginBukkit) Bukkit.getPluginManager().getPlugin("FastLogin");
+                    if (fastLogin != null) {
+                        PremiumStatus premiumStatus = fastLogin.getStatus(myPlayer.getUniqueId());
+                        if (premiumStatus == PremiumStatus.PREMIUM) {
+                            if (plugin.debug()) {
+                                plugin.getLogger().info("Skipping player " + myPlayer.getName() + " because they are premium.");
                             }
+                            return;
                         }
                     }
                 }
+            }
 
-                plugin.getNotLoggedIn().add(myPlayer.getUniqueId());
-                new AnvilGUI.Builder()
-                        .onComplete((player, text) -> {
-                            if (plugin.isAuthme() && plugin.getConfig().getBoolean("register") && !AuthMeApi.getInstance().isRegistered(player.getName())) {
-                                AuthMeApi.getInstance().forceRegister(player, text, true);
-                                plugin.getLoggedIn().add(player.getUniqueId());
-                                plugin.getNotLoggedIn().remove(player.getUniqueId());
-                                if (plugin.getConfig().getBoolean("login_messages")) {
-                                    Translations.LOGGED_IN.send(player);
-                                }
-                                return AnvilGUI.Response.close();
+            plugin.getNotLoggedIn().add(myPlayer.getUniqueId());
+            AnvilGUI.Builder anvilGUI = new AnvilGUI.Builder()
+                    .onComplete((player, text) -> {
+                        if (plugin.isAuthme() && plugin.getConfig().getBoolean("register") && !AuthMeApi.getInstance().isRegistered(player.getName())) {
+                            AuthMeApi.getInstance().forceRegister(player, text, true);
+                            plugin.getLoggedIn().add(player.getUniqueId());
+                            plugin.getNotLoggedIn().remove(player.getUniqueId());
+                            if (plugin.getConfig().getBoolean("login_messages")) {
+                                Translations.LOGGED_IN.send(player);
                             }
-
-                            if (text.equalsIgnoreCase(plugin.getConfig().getString("Password"))
-                                    || (plugin.isAuthme() && AuthMeApi.getInstance().checkPassword(player.getName(), text))) {
-                                plugin.getLoggedIn().add(player.getUniqueId());
-                                plugin.getNotLoggedIn().remove(player.getUniqueId());
-                                if (plugin.getConfig().getBoolean("login_messages")) {
-                                    Translations.LOGGED_IN.send(player);
-                                }
-                                if (plugin.isAuthme()) AuthMeApi.getInstance().forceLogin(player);
-                                player.setLevel(player.getLevel());
-                                return AnvilGUI.Response.close();
-                            } else {
-                                return AnvilGUI.Response.text(Translations.GUI_WRONG.get(myPlayer));
-                            }
-                        })
-                        .preventClose()
-                        .text(Translations.GUI_TEXT.get(myPlayer))
-                        .itemLeft(new ItemStack(Material.ANVIL))
-                        .title(Translations.GUI_TITLE.get(myPlayer))  //only works in 1.14+
-                        .plugin(plugin)
-                        .open(myPlayer);
-
-
-                if (plugin.getConfig().getBoolean("Timeout")) {
-                    Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                        if (!plugin.getLoggedIn().contains(myPlayer.getUniqueId())) {
-                            myPlayer.kickPlayer(Translations.KICKED.get(myPlayer));
+                            return AnvilGUI.Response.close();
                         }
-                    }, plugin.getConfig().getLong("Time"));
-                }
-            }, 40L);
+
+                        if (text.equalsIgnoreCase(plugin.getConfig().getString("Password"))
+                                || (plugin.isAuthme() && AuthMeApi.getInstance().checkPassword(player.getName(), text))) {
+                            plugin.getLoggedIn().add(player.getUniqueId());
+                            plugin.getNotLoggedIn().remove(player.getUniqueId());
+                            if (plugin.getConfig().getBoolean("login_messages")) {
+                                Translations.LOGGED_IN.send(player);
+                            }
+                            if (plugin.isAuthme()) AuthMeApi.getInstance().forceLogin(player);
+                            player.setLevel(player.getLevel());
+                            return AnvilGUI.Response.close();
+                        } else {
+                            return AnvilGUI.Response.text(Translations.GUI_WRONG.get(myPlayer));
+                        }
+                    })
+                    .preventClose()
+                    .text(Translations.GUI_TEXT.get(myPlayer))
+                    .itemLeft(new ItemStack(Material.ANVIL))
+                    .title(Translations.GUI_TITLE.get(myPlayer))  //only works in 1.14+
+                    .plugin(plugin);
+            Bukkit.getScheduler().runTaskLater(plugin, () -> anvilGUI.open(myPlayer), 20L);
+
+            if (plugin.getConfig().getBoolean("Timeout")) {
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    if (!plugin.getLoggedIn().contains(myPlayer.getUniqueId())) {
+                        myPlayer.kickPlayer(Translations.KICKED.get(myPlayer));
+                    }
+                }, plugin.getConfig().getLong("Time"));
+            }
+        }
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (plugin.getNotLoggedIn().contains(player.getUniqueId())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onClick(InventoryClickEvent event) {
+        Player player = (Player) event.getView().getPlayer();
+        if (plugin.getNotLoggedIn().contains(player.getUniqueId())) {
+            event.setCancelled(true);
         }
     }
 
