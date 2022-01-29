8bcd76c7d2918508f2b508e9066d28aa5f948a7d
SamB440 <sam@islandearth.net>
2022-01-29 19:01:28 +0000

    Update to 1.18.1 + all dependencies, fixes #6

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("java")
+}
+
+group = "net.islandearth"
+version = "1.1.3"
+
+repositories {
+    mavenCentral()
+
+    maven("https://erethon.de/repo/")
+    maven("https://repo.convallyria.com/snapshots")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+
+    maven {
+        name = "codemc-snapshots"
+        url = uri("https://repo.codemc.io/repository/maven-snapshots/")
+    }
+
+    flatDir { dirs("libraries") } // FastLogin
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+
+    compileOnly("org.spigotmc:spigot-api:1.18.1-R0.1-SNAPSHOT")
+    compileOnly("fr.xephi:authme:5.6.0-SNAPSHOT")
+    compileOnly(":FastLoginBukkit")
+
+    implementation("net.wesjd:anvilgui:1.5.3-SNAPSHOT") // anvilgui
+    implementation("net.islandearth:languagy:2.0.4-SNAPSHOT") // languagy
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    shadowJar {
+        relocate("net.islandearth.languagy", "net.islandearth.anvillogin.libs.languagy")
+        relocate("net.wesjd.anvilgui", "net.islandearth.anvillogin.libs.anvilgui")
+
+        archiveClassifier.set("")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        filesMatching("plugin.yml") {
+            expand("version" to version)
+        }
+    }
+
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
